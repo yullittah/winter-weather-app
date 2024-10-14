8d189a45782fccc60af1a9cd40917bf83148e9e2
yullittah <yullittahmakuwe@gmail.com>
2024-10-14 17:24:17 +0200

    added link to github repo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ export default function App() {
   return (
     <div className="App">
       <h1>weather app</h1>
+
+      <footer>
+        project is open sourced on{" "}
+        <a
+          href="https://github.com/yullittah/winter-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>{" "}
+        coded by Yullittah
+      </footer>
     </div>
   );
 }
