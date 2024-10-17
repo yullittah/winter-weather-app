31d4ee5ffafc9df32df2363eaf257da48b6e1430
yullittah <yullittahmakuwe@gmail.com>
2024-10-17 20:02:48 +0200

    added css to the weather.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import WeatherApp from "./weather";
+import "./weather.css";
+import "./App.css";
 function App() {
-  return <div className="App"> </div>;
+  return (
+    <div className="App">
+      <WeatherApp />
+      <footer>
+        coded by Yullittah and open sourced on{" "}
+        <a
+          href="https://github.com/yullittah/weather-app-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>{" "}
+      </footer>{" "}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/weather.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./weather.css";
+import "./App.css";
+export default function Weather() {
+  return (
+    <div className="app">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            {" "}
+            <input
+              type="search"
+              placeholder="Enter a city . . ."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Weather App </h1>
+      <ul>
+        <li>Wednesday 0700hrs</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              className="float-left"
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="cloudy weather icon"
+            />
+            <span className="Temperature">15 </span>
+            <span className="Units">°C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation 15mm</li>
+            <li>wind 10km/hr</li>
+            <li>humidity 98%</li>
+            <li>Temperature 15°C</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
